60a949bda60c837cbba3b8e283f08b01166f34ea
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-08-06 14:38:28 +0200

    Add files via upload

---
+++ EvAM-MDR.R
@@ -0,0 +1,38 @@
+## example of evolutionary accumulation modelling (EvAM) for multidrug resistance (MDR)
+# uses HyperTraPS-CT to explore tuberculosis MDR evolution
+
+library(readxl)       # to read Excel datafile
+library(dplyr)        # for data wrangling
+
+# get this from https://github.com/StochasticBiology/hypertraps-ct
+source("hypertraps.R")
+
+# get profiles of drug resistance/susceptibility
+# Supplementary Table 4 of Casali et al., https://www.nature.com/articles/ng.2878.s3
+# https://static-content.springer.com/esm/art%3A10.1038%2Fng.2878/MediaObjects/41588_2014_BFng2878_MOESM35_ESM.xls
+o.df = read_excel("41588_2014_BFng2878_MOESM35_ESM.xls")
+
+# extract isolate ID and resistance profiles to our ten drugs (discarding mutation info)
+# remove any incomplete profiles and recast as binary strings
+col.interest = c("Isolate", "INH", "RIF", "PZA", "EMB", "STR", "AMI", "CAP", "MOX", "OFL", "PRO")
+df = o.df[,which(colnames(o.df) %in% col.interest)]
+missing.rows = unique(which(df == ".", arr.ind=TRUE)[,1])
+final.df = df[-missing.rows,]
+final.df = final.df %>%
+  mutate(across(-Isolate, ~ ifelse(. == "R", 1, 0)))
+final.df = as.data.frame(final.df)
+
+# get phylogeny linking isolates
+# Supplementary Data Set 1 of Casali et al., https://www.nature.com/articles/ng.2878.s3
+# https://static-content.springer.com/esm/art%3A10.1038%2Fng.2878/MediaObjects/41588_2014_BFng2878_MOESM34_ESM.txt
+tree = read.tree("41588_2014_BFng2878_MOESM34_ESM.txt")
+
+# reconstruct ancestral states using parsimony picture and extract before-after transitions
+src.data = curate.tree(tree, final.df)
+
+# fit model using HyperTraPS-CT (will take an hour or so with this parameterisation)
+fitted.model = HyperTraPS(src.data$dests, 
+                          initialstates = src.data$srcs, 
+                          starttimes = src.data$times*1000, endtimes = src.data$times*1000, 
+                          penalty = 1, seed = 1, length = 5, kernel = 3)
+
